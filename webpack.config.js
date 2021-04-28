eb8fa51dd52e62cdd80840bc0b2c3fa68ff63c5f
Josehower <josehowerupn@gmail.com>
2021-04-28 13:10:22 +0200

    Create first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
+};
